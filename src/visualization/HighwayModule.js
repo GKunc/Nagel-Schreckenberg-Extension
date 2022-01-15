18de99d2b674924bf2d05a62612bf383822e1c19
Grzegorz Kunc <grzeg.kunc@gmail.com>
2022-01-15 09:48:44 +0100

    Small refactor

--- src/visualization/HighwayModule.js
+++ src/visualization/HighwayModule.js
@@ -1,47 +1,26 @@
 var HighwayModule = function(canvas_width, canvas_height) {
-    var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
+    let canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
     canvas_tag += "style='border:1px dotted'></canvas>";
-    var canvas = $(canvas_tag)[0];
+    let canvas = $(canvas_tag)[0];
     $("#elements").append(canvas);
-    var context = canvas.getContext("2d");
+    let context = canvas.getContext("2d");
 
-    // Create the chart object
     this.render = function(data) {
         console.log(data)
-        context.strokeStyle = 'black';
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.rect(0, 0, 3000, 30);
-        context.rect(0, 30, 3000, 30);
-        context.setLineDash([5, 10]);
-        context.stroke();
+        createLanes();
 
-        for(var i=0; i<data.length; i++) {
+        for(let i=0; i<data.length; i++) {
             if(data[i].type === "Truck") {
-                context.fillStyle = 'green';
-                context.fillRect(i*50, 5, 45, 20);
+                createTruckAgent(i);
             }
             if(data[i].type === "FamilyCar") {
-                context.fillStyle = 'blue';
-                context.fillRect(i*50, 5, 35, 20);
+                createFamilyCarAgent(i);
             }
             if(data[i].type === "SportsCar") {
-                context.fillStyle = 'red';
-                context.fillRect(i*50, 5, 30, 20);
+                createSportsCarAgent(i);
             }
             if(data[i].type === "Construction") {
-                context.beginPath();
-                context.moveTo(i*50, 2);
-                context.lineTo(i*50+48, 28);
-                context.moveTo(i*50, 28);
-                context.lineTo(i*50+48, 2);
-                context.strokeStyle = 'red';
-                context.setLineDash([]);
-                context.stroke();
-                context.fillStyle = 'black';
-                context.fillRect(i*50, 2, 48, 26);
-                context.stroke();
-
-                // context.fillText("blah", i*60, 15);
+                createConstructionAgent(i);
             }
         }
     };
@@ -50,4 +29,42 @@ var HighwayModule = function(canvas_width, canvas_height) {
         context.strokeStyle = 'black';
         context.clearRect(0, 0, canvas.width, canvas.height);
     };
+
+    function createLanes() {
+        context.strokeStyle = 'black';
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.rect(0, 0, 3000, 30);
+        context.rect(0, 30, 3000, 30);
+        context.setLineDash([5, 10]);
+        context.stroke();
+    }
+
+    function createTruckAgent(i) {
+        context.fillStyle = 'green';
+        context.fillRect(i*50, 5, 45, 20);
+    }
+
+    function createFamilyCarAgent(i) {
+        context.fillStyle = 'blue';
+        context.fillRect(i*50, 5, 35, 20);
+    }
+
+    function createSportsCarAgent(i) {
+        context.fillStyle = 'red';
+        context.fillRect(i*50, 5, 30, 20);
+    }
+
+    function createConstructionAgent(i) {
+         context.beginPath();
+         context.moveTo(i*50, 2);
+         context.lineTo(i*50+48, 28);
+         context.moveTo(i*50, 28);
+         context.lineTo(i*50+48, 2);
+         context.strokeStyle = 'red';
+         context.setLineDash([]);
+         context.stroke();
+         context.fillStyle = 'black';
+         context.fillRect(i*50, 2, 48, 26);
+         context.stroke();
+    }
 };
